d033e3f8ea6356ce70baf532d5e819cfdcb9fc12
wandiao <851298395@qq.com>
2017-11-07 15:15:24 +0800

    自动化部署

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const http = require('http')
 const app = express()
+const config = require('./local_config');
 
 // 跨域设置
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Origin", config.reqUrl)
   res.header("Access-Control-Allow-Headers", "X-Requested-With")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("X-Powered-By", ' 3.2.1')
